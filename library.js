e86d5340481834be9cdee5b53a5d4b921ed6ebb1
D33ZU11 <deezull.uk@gmail.com>
2026-04-16 19:12:35 +0100

    Create library.js

---
+++ library.js
@@ -0,0 +1,13 @@
+// This file holds your permanent data. 
+// Adding things here ensures they never disappear!
+const INITIAL_DB = {
+    "Time": [
+        { id: "t1", label: 'Wait', img: 'https://cdn-icons-png.flaticon.com/512/2997/2997985.png' }
+    ],
+    "Social": [
+        { id: "s1", label: 'Help', img: 'https://cdn-icons-png.flaticon.com/512/1182/1182740.png' }
+    ],
+    "My Cards": [
+        { id: "custom1", label: 'Example', img: 'https://via.placeholder.com/150' }
+    ]
+};
